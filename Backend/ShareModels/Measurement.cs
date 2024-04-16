468a59d8608354332b780030687605e1488661ac
Mikkel Theut <mikkeltheut@gmail.com>
2024-04-16 11:11:06 +0200

    Making all the work.

---
+++ Backend/ShareModels/Measurement.cs
@@ -0,0 +1,10 @@
+﻿namespace ShareModels
+{
+    public class Measurement
+    {
+        public int Id { get; set; }
+        public DateTime date { get; set; }
+        public int Systolic { get; set; }
+        public int Diastolic { get; set; }
+    }
+}
---
+++ Backend/ShareModels/Patient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShareModels
+{
+    public class Patient
+    {
+        public string ssn { get; set; }
+        public string mail { get; set; }
+        public string name { get; set; }
+        public Measurement[] measurements { get; set; }
+    }
+}
