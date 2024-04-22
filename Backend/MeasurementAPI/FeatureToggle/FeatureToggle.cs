9d58c6ad70b87156948973f68f3e861ec8366e0e
Mikkel Theut <mikkeltheut@gmail.com>
2024-04-22 11:24:11 +0200

    Added Mikkel Key

--- Backend/MeasurementAPI/FeatureToggle/FeatureToggle.cs
+++ Backend/MeasurementAPI/FeatureToggle/FeatureToggle.cs
@@ -7,13 +7,13 @@ public class FeatureToggle: IFeatureToggle
 {
     EdgeFeatureHubConfig _config = null;
     private string _janKey = "7aa23a64-7cc0-43a1-820e-a0dd48c2ce5f/JLT9WVT6k5z4cuQlIH8LdILLf3uBcacZZUfqn5D5";
-    private string _mikkelKEy = "default";
+    private string _mikkelKEy = "d56a33fb-93cd-4afa-ac35-c592b4541be2/yOcmL3wxTTWGD143avq0KFTsjDhBUVK30mrfvD7o";
     private string _simonKEy = "default";
     
     
     public FeatureToggle()
     {
-        _config = new EdgeFeatureHubConfig("http://featurehub:8085", _janKey);
+        _config = new EdgeFeatureHubConfig("http://featurehub:8085", _mikkelKEy);
     }
     
     public async Task<bool> IsCountryAllowed(string country)
