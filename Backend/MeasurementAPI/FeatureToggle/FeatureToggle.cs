ab2ca0c01d53484ab32cb1108e2ca041a986bbb9
Tadiaki <n0m1z@hotmail.com>
2024-04-22 11:29:04 +0200

    Doctores y patientes virker

--- Backend/MeasurementAPI/FeatureToggle/FeatureToggle.cs
+++ Backend/MeasurementAPI/FeatureToggle/FeatureToggle.cs
@@ -8,12 +8,12 @@ public class FeatureToggle: IFeatureToggle
     EdgeFeatureHubConfig _config = null;
     private string _janKey = "7aa23a64-7cc0-43a1-820e-a0dd48c2ce5f/JLT9WVT6k5z4cuQlIH8LdILLf3uBcacZZUfqn5D5";
     private string _mikkelKEy = "default";
-    private string _simonKEy = "default";
+    private string _simonKEy = "005c0035-ef49-4bb4-82c2-63e81dead015/oz0QAq7IZIVtkROqLR0csh2lrKVlsxr0ivQnaKqF";
     
     
     public FeatureToggle()
     {
-        _config = new EdgeFeatureHubConfig("http://featurehub:8085", _janKey);
+        _config = new EdgeFeatureHubConfig("http://featurehub:8085", _simonKEy);
     }
     
     public async Task<bool> IsCountryAllowed(string country)
